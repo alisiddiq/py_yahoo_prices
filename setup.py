ec347dd777e4e0f4ce9b153d8478900a467af9e4
alisiddiq <ali.bin.siddiq@gmail.com>
2018-05-26 18:17:41 +0100

    changed description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='py_yahoo_prices',
     packages=find_packages(),
     version='0.4.2',
-    description="Get daily/weekly/monthly returns and prices of equities from yahoo's new tricky endpoint",
+    description="Get daily/weekly/monthly prices of equities from yahoo's new tricky endpoint",
     long_description='README.md',
     author = 'Ali Siddiq',
     author_email = 'ali.bin.siddiq@gmail.com',
